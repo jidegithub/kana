eefe41a795c9b736c19aae5d66b4e62599ca9527
olajide.o <olajide.o@ng.lopworks.com>
2020-11-11 17:54:04 +0100

    update: removes unused lines

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,29 +1,17 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 import Proptypes from 'prop-types';
 import { decrementPage, incrementPage } from '../../actions';
 import './Pagination.scss'
 
 
-function Pagination({ page, incrementPage, decrementPage, pageMax}) {
-  // const [count, setCount] = useState(0)
-
-  // const handleIncrement = () =>
-  //   setCount(currentCount => currentCount + 1);
-
-  // const handleDecrement = () =>
-  //   setCount(currentCount => currentCount - 1);
-
-  // useEffect(() => setCount(currentCount => currentCount + 1), []);
-
-  const disableNext = () => page === pageMax ? null : incrementPage 
+function Pagination({ page, incrementPage, decrementPage}) {
   
 
+  const disableNext = () => page === -1 ? null : incrementPage 
+  
   const disablePrevious = () => page === 1 ? null : decrementPage
 
-  useEffect(() => {
-    // setPageMax(page_max_rounded)
-  }, [page, pageMax])
 
   return (
     <div className="pagination-main-info flex flex-justify-between">
@@ -31,7 +19,7 @@ function Pagination({ page, incrementPage, decrementPage, pageMax}) {
         <p onClick={disablePrevious()}>Previous</p>
       </span>
       <span>
-      <p><span className="current_page_display">{page}</span> of {pageMax}</p>
+      <p><span className="current_page_display">{page}</span> of 14</p>
       </span>
       <span className="next-btn">
         <p onClick={disableNext()} className="switch_next_container">
@@ -52,12 +40,10 @@ Pagination.propTypes = {
   incrementPage: Proptypes.func.isRequired,
   decrementPage: Proptypes.func.isRequired,
   page: Proptypes.number.isRequired,
-  pageMax: Proptypes.number.isRequired
 }
 
 const mapStateToprops = (state) => ({
   page: state.templates.page,
-  pageMax: state.templates.pageMax
 })
 
 export default connect(mapStateToprops, {incrementPage, decrementPage})(Pagination);
