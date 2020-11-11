399b957ddf52c93428e71bbdf650f579af426631
olajide.o <olajide.o@ng.lopworks.com>
2020-11-11 17:49:18 +0100

    update: updat templates

--- src/components/Templates/Templates.jsx
+++ src/components/Templates/Templates.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, Suspense, lazy, memo } from 'react'
 import short from 'short-uuid';
 import CurrentTemplatesInfo from '../CurrentTemplatesInFo/CurrentTemplatesInfo'
-import Proptypes  from 'prop-types';
+import Proptypes from 'prop-types';
 import { connect } from 'react-redux';
 import './Templates.scss';
-import { fetchTemplates, setPageMax } from '../../actions/index';
+import { fetchTemplates } from '../../actions/index';
 const Template = lazy(() => import('../Template/Template'));
 
 
-function Templates({ unModifiedTemplates, fetchTemplates, page}) {
-  
+function Templates({ unModifiedTemplates, fetchTemplates, page }) {
+
   const templatesList = unModifiedTemplates.map(function (template) {
-    return(
+    return (
       <Template
         key={short.generate()}
         name={template.name}
@@ -24,19 +24,15 @@ function Templates({ unModifiedTemplates, fetchTemplates, page}) {
 
   useEffect(() => {
     fetchTemplates()
-  }, [fetchTemplates, unModifiedTemplates])
-
-  useEffect(() => {
-    setPageMax(page_max_rounded)
-  }, [unModifiedTemplates, page_max_rounded])
+  }, [fetchTemplates])
 
   return (
     <section className="templates-grand-container">
-      <CurrentTemplatesInfo/>
+      <CurrentTemplatesInfo />
       <div className="templates-grid">
         <Suspense fallback={templateListUnavailable()}>
-          {chunkedTemplates.length ? templatesList : 
-        <div style={{marginLeft: 10}}><p>No templates found! try filtering.</p></div>}
+          {unModifiedTemplates.length ? templatesList :
+            <div style={{ marginLeft: 10 }}><p>No templates found! try filtering.</p></div>}
         </Suspense>
       </div>
     </section>
@@ -46,8 +42,6 @@ function Templates({ unModifiedTemplates, fetchTemplates, page}) {
 Templates.propTypes = {
   fetchTemplates: Proptypes.func.isRequired,
   unModifiedTemplates: Proptypes.array.isRequired,
-  page: Proptypes.number.isRequired,
-  setPageMax: Proptypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
@@ -56,4 +50,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default memo(connect(mapStateToProps, { fetchTemplates })(Templates));
-
